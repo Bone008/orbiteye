5a86b2e7394e53f96df2f946158f417a05ae7a7d
Sahil Patel <sahilshahpatel@gmail.com>
2022-02-21 23:15:08 +0100

    Fixed bug with zoom distances. Switched to Trackball controls to enable rotating past poles.

--- src/components/GlobeView.tsx
+++ src/components/GlobeView.tsx
@@ -1,21 +1,21 @@
 import './GlobeView.css'
 
 import { Canvas } from '@react-three/fiber'
-import { Center, OrbitControls, Sphere, useTexture } from '@react-three/drei'
+import { Center, Sphere, TrackballControls, useTexture } from '@react-three/drei'
 import { useRef, Suspense } from 'react';
 import { DefaultValues } from '../util/optional_props';
 
 
 export interface GlobeViewProps {
   radius?: number,
-  minDistance?: number,
+  minDistance?: number, // Must be <= radius
   maxDistance?: number,
 };
 
 const defaultProps: DefaultValues<GlobeViewProps> = {
   radius: 1,
-  minDistance: 10,
-  maxDistance: 2,
+  minDistance: 1.5,
+  maxDistance: 10,
 }
 
 
@@ -27,7 +27,7 @@ export default function GlobeView(__props: GlobeViewProps) {
       <Suspense fallback={null}> {/* Replaces canvas with nothing while loading */}
         <Canvas>
           <ambientLight />
-          <OrbitControls maxDistance={props.maxDistance} minDistance={props.maxDistance} enablePan={false} />
+          <TrackballControls maxDistance={props.maxDistance} minDistance={props.minDistance} />
           <Globe {...props} />
         </Canvas>
       </Suspense >
