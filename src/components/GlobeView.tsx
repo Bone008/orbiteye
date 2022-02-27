43ecf911e6be9f8b48c3e974d3d81e08e22b5734
Sahil Patel <sahilshahpatel@gmail.com>
2022-02-27 12:03:48 +0100

    #44 Added hover interaction for 3D orbits in a performant manner

--- src/components/GlobeView.tsx
+++ src/components/GlobeView.tsx
@@ -1,8 +1,8 @@
 import './GlobeView.css'
 
-import { Canvas, useFrame } from '@react-three/fiber'
+import { Canvas, ThreeEvent, useFrame } from '@react-three/fiber'
 import { Line, Sphere, Stars, TrackballControls, useTexture } from '@react-three/drei'
-import { useRef, Suspense } from 'react';
+import { useRef, Suspense, useState } from 'react';
 import { DefaultValues } from '../util/optional_props';
 import { Satellite } from '../model/satellite';
 import { getOrbitECI } from '../util/orbits';
@@ -19,14 +19,16 @@ export interface GlobeViewProps {
   minDistance?: number, // Must be <= radius
   maxDistance?: number,
   orbitOpacity?: number,
+  orbitLineWidth?: number,
 };
 
 const defaultProps: DefaultValues<GlobeViewProps> = {
-  orbitLimit: 10,
+  orbitLimit: 20,
   radius: 1,
   minDistance: 1.5,
   maxDistance: 15,
   orbitOpacity: 1,
+  orbitLineWidth: 2,
 }
 
 
@@ -40,7 +42,7 @@ export default function GlobeView(__props: GlobeViewProps) {
           <color attach="background" args={["black"]} />
           <ambientLight />
           <TrackballControls maxDistance={props.maxDistance} minDistance={props.minDistance} noPan />
-          <Globe {...props} />
+          <SceneObjects {...props} />
           <Stars fade />
         </Canvas>
       </Suspense >
@@ -50,7 +52,7 @@ export default function GlobeView(__props: GlobeViewProps) {
 
 
 // Separate from parent because R3F hooks must be used inside a Canvas tag
-function Globe(props: Required<GlobeViewProps>) {
+function SceneObjects(props: Required<GlobeViewProps>) {
   const sphereRef = useRef<THREE.Mesh>(null!);
   const texture = useTexture({ map: 'assets/NASA-Visible-Earth-September-2004.jpg' });
 
@@ -58,57 +60,55 @@ function Globe(props: Required<GlobeViewProps>) {
   //  - this reduces assumption that you can look at 3D orbits to see where they fly over the surface
   useFrame((state, delta) => { sphereRef.current.rotation.y += 2 * Math.PI * delta / 60; })
 
+
+  // Block hover events from orbits behind Earth
+  const occludeHover = {
+    onPointerEnter: (e: ThreeEvent<PointerEvent>) => { e.stopPropagation(); },
+    onPointerLeave: (e: ThreeEvent<PointerEvent>) => { e.stopPropagation(); }
+  };
+
   const satellites = props.filteredSatellites.filter(sat => !!sat.tle).slice(0, props.orbitLimit);
-  const orbits = satellites.map(sat => {
-    const coordinates = getOrbitECI(sat);
-
-    // Note on the commented section below:
-    //  Interaction with these elements seems pretty rough. Performance slowed down a ton when I tried to use it.
-    //  Additionally, the typing from React-Three-Fiber seems wrong because it won't let me access the object's material
-    //  through the event, but online tutorials suggest that I can (and disabling TS shows that I can for sure).
-    //  I've commented it all out for now (and removed the props from the Line element) since none of it was working
-    //  in a usable way. Still, I wanted to keep it as a good place to start next time.
-
-    // const onPointerEnter = (e: ThreeEvent<PointerEvent>) => {
-    //   // @ts-ignore (type definitions appear not to mention that you can access the material as a LineMaterial)
-    //   e.object.material.opacity = 1;
-
-    //   // @ts-ignore
-    //   // e.object.material.color = new Color("blue"); // Colors don't seem to work at all
-    // };
-
-    // const onPointerLeave = (e: ThreeEvent<PointerEvent>) => {
-    //   // @ts-ignore (type definitions appear not to mention that you can access the material as a LineMaterial)
-    //   e.object.material.opacity = props.orbitOpacity;
-
-    //   // @ts-ignore
-    //   // e.object.material.color = new Color("red"); // Colors don't seem to work at all
-    // };
-
-    const scale_inv = EARTH_RADIUS_KM / props.radius;
-    for (let i = 0; i < coordinates.length; i++) {
-      coordinates[i].divideScalar(scale_inv);
-    }
-
-
-    // TODO: Opacity < 1 doesn't work properly (parts of the line appear in full opacity, others at the selected value)
-    const material = {
-      color: "red",
-      transparent: props.orbitOpacity !== 1,
-      opacity: props.orbitOpacity,
-    }
-
-    return <Line key={sat.id} name={sat.id} points={coordinates} {...material} onClick={e => console.log(e.object.name)} />
-
-  });
+  const orbits = satellites.map(sat => <Orbit key={sat.id} satellite={sat} {...props} />);
 
   return (
     <>
       {/* Sphere rotation aligns the Z axis with ECI coordinates! */}
-      <Sphere ref={sphereRef} args={[props.radius, 64, 32]} rotation={new Euler(Math.PI / 2, 0, 0)}>
+      <Sphere ref={sphereRef} args={[props.radius, 64, 32]} rotation={new Euler(Math.PI / 2, 0, 0)} {...occludeHover}>
         <meshLambertMaterial {...texture} />
       </Sphere>
       {orbits}
     </>
   );
+}
+
+
+type OrbitProps = { satellite: Satellite } & Required<GlobeViewProps>;
+function Orbit(props: OrbitProps) {
+  const sat = props.satellite;
+
+  const [hovered, setHovered] = useState<boolean>(false);
+  const coordinates = getOrbitECI(sat);
+
+  const scale_inv = EARTH_RADIUS_KM / props.radius;
+  for (let i = 0; i < coordinates.length; i++) {
+    coordinates[i].divideScalar(scale_inv);
+  }
+
+  // TODO: Opacity < 1 doesn't work properly (parts of the line appear in full opacity, others at the selected value)
+  const material = {
+    color: hovered ? "green" : "red",
+    transparent: props.orbitOpacity !== 1,
+    opacity: props.orbitOpacity,
+    lineWidth: props.orbitLineWidth,
+  }
+
+  const hoverControls = {
+    onPointerEnter: (e: ThreeEvent<PointerEvent>) => { setHovered(true); e.stopPropagation(); },
+    onPointerLeave: (e: ThreeEvent<PointerEvent>) => { setHovered(false); e.stopPropagation(); }
+  };
+
+  // TODO: link this onclick with the Details panel
+  const onclick = (e: ThreeEvent<MouseEvent>) => console.log(`Selected ${e.object.name}`);
+
+  return <Line name={sat.id} points={coordinates} {...material} {...hoverControls} onClick={onclick} />
 }
