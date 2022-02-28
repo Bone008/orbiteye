a5c2a8c0796e4f4a1513c799057becb605a4b3ec
Sahil Patel <sahilshahpatel@gmail.com>
2022-02-28 20:24:28 +0100

    Added GeoJSON types. Fixed a few issues on SVGWorldMap -- still some things to change further.

--- src/components/SVGWorldMap.tsx
+++ src/components/SVGWorldMap.tsx
@@ -2,8 +2,8 @@ import './SVGWorldMap.css';
 import { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
 import { Satellite } from '../model/satellite';
-import { groundTraceSync } from '../util/orbits';
 import { DefaultValues } from '../util/optional_props';
+import { Feature } from 'geojson';
 
 export interface WorldMapProps {
   filteredSatellites: Satellite[];
@@ -37,97 +37,64 @@ export default function WorldMap(reqProps: WorldMapProps) {
   // Projections path generator
   const pathGenerator = d3.geoPath().projection(mapProjection);
 
-  //Color scale
+  // Color scale
   const colorScale = d3.scaleLog()
-    .range(["lightgreen", "green"] as Iterable<number>)
+    .range(["lightgreen", "green"] as Iterable<number>)  //TODO: Why Iterable number
     .domain([1, 100])
 
-  // Render world map (runs just once)
+  // Render world map
   useEffect(() => {
     // Loading the world map
+    // TODO: probably shouldn't fetch on every single load -- instead fetch just once (maybe using useState?)
     fetch('data/world.json').then(resp => {
       resp.json().then(json => {
 
         //Computing the number of satellite per country within the filtered satellites dataset
-        var nbSatellitePerCountry = d3.rollup(props.filteredSatellites, v => d3.sum(v, d => 1), d => d.owner)
+        const nbSatellitePerCountry = d3.rollup(props.filteredSatellites, v => d3.sum(v, d => 1), d => d.owner);
 
         // Creating the path to make the map
-        var groupMap = mapLayer.selectAll("path")
-          .data(json.features)
+        mapLayer.selectAll("*").remove();
+        const groupMap = mapLayer.selectAll("path")
+          .data(json.features as Feature[])
 
         groupMap
           .enter()
           .append('path')
-          .merge(groupMap as any)
-          .transition()
-          .duration(600)
-          .attr("d", function (d: any) { return pathGenerator(d) })
-          .attr("stroke", "grey")
-          .attr("stroke-width", "1px")
-          .attr("fill", function (d: any) {
-            //If undefined/unknown, we put it in grey
-            if (!nbSatellitePerCountry.get(d.properties.ISO_A3)) return "lightgrey"
-            //console.log("data", nbSatellitePerCountry.get(d.properties.ISO_A3), d.properties.ISO_A3)
 
-            //Otherwise we put it in a color scale TODO check with mapping
-            return colorScale(nbSatellitePerCountry.get(d.properties.ISO_A3) as number)
-          })
-          //Some mouse interactions
-          .on('mouseover', function (d: any) {
-            d3.select(d.srcElement)
+          // Some mouse interactions
+          .on('mouseover', e => {
+            d3.select(e.srcElement)
               .style("opacity", .8)
               .style("stroke", "black")
           })
-          .on('mouseout', function (d: any) {
-            d3.select(d.srcElement)
+          .on('mouseout', e => {
+            d3.select(e.srcElement)
               .style("opacity", 1)
               .style("stroke", "grey")
           })
 
-        //For updating
-        groupMap.exit().remove()
+          .transition()     // TODO: not working either. May be related to exit().remove() stuff
+          .duration(600)
+          .attr("d", d => pathGenerator(d))
+          .attr("stroke", "grey")
+          .attr("stroke-width", "1px")
+          .attr("fill", d => {
+            // If undefined/unknown, we put it in grey
+            if (!nbSatellitePerCountry.get(d.properties?.ISO_A3)) return "lightgrey"
+            //console.log("data", nbSatellitePerCountry.get(d.properties.ISO_A3), d.properties.ISO_A3)
+
+            // Otherwise we put it in a color scale TODO check with mapping
+            return colorScale(nbSatellitePerCountry.get(d.properties?.ISO_A3) as number)
+          })
+
+
+        // For updating
+        // TODO: why is this not working? We shouldn't need to use selectAll("*").remove()
+        // groupMap.exit().remove()
       });
     });
 
   }, [props.filteredSatellites, mapProjection]);
-
-  // D3 logic goes here and will run anytime an item in the second argument is modified (shallow comparison)
-  useEffect(() => {
-    const traceLayer = d3.select(svgRef.current).select("g.traceLayer");
-
-    // Reset the container
-    traceLayer.selectAll("*").remove();
-
-    // Filter to satellites with orbital data
-    const satellites = props.filteredSatellites.filter(sat => !!sat.tle).slice(0, props.traceLimit);
-
-    // Calculate all traces
-    traceLayer.selectAll("path")
-      .data(satellites)
-      .enter()
-      .append("path")
-      .attr("d", sat => {
-        const trace: [number, number][] = groundTraceSync(sat).map(lngLat => [lngLat[0], lngLat[1]]);
-        const lineGen = d3.line()
-          .x(p => p[0])
-          .y(p => p[1])
-        return lineGen(trace.map(p => mapProjection(p) || [Infinity, Infinity]));
-      })
-      .attr("fill", "none")
-      .attr("stroke", "red") // TODO: base on something else
-      .attr("stroke-width", "1px")
-      .attr("border-width", "2px")
-      .attr("border-color", "blue")
-      .on('mouseover', d => {
-        d3.select(d.srcElement)
-          .style("stroke-width", "10px");
-      })
-      .on('mouseout', d => {
-        d3.select(d.srcElement)
-          .style("stroke-width", "1px");
-      })
-
-  }, [props.filteredSatellites, props.traceLimit, mapProjection]);
 
   // Set up zoom and panning
   const zoom = d3.zoom()
@@ -152,7 +119,6 @@ export default function WorldMap(reqProps: WorldMapProps) {
         className="WorldMap" style={{ backgroundColor: "lightblue", padding: "0" }}
       >
         <g className="mapLayer"></g>
-        <g className="traceLayer"></g>
       </svg>
     </div>
   );
