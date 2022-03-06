fd085b3981cace137e5f73a9c29ac9fe4f82cc92
Sahil Patel <sahilshahpatel@gmail.com>
2022-03-06 15:14:23 +0100

    Adding updated content to view selector (background images and new descriptions). #65

--- src/components/ViewSelector.tsx
+++ src/components/ViewSelector.tsx
@@ -22,8 +22,19 @@ export default function ViewControls(props: ViewControlsProps) {
   }
 
   const optionBlocks = viewOptions.map(opt => {
+    // Set up background image
+    const style = {
+      "--backgroundURL": `url(/assets/Mode${opt.name}.png)`
+    }
+
     return (
-      <NavLink key={opt.name + "_block"} className={({ isActive }) => "optionBlock" + (isActive ? ' selected' : '')} to={opt.href} onClick={showBot}>
+      <NavLink
+        key={opt.name + "_block"}
+        className={({ isActive }) => "optionBlock" + (isActive ? ' selected' : '')}
+        to={opt.href}
+        onClick={showBot}
+        style={style as React.CSSProperties}
+      >
         <div className="optionContainer">
           <p className="optionTitle">{opt.name}</p>
           <p className="optionDescription">{opt.description}</p>
@@ -42,11 +53,20 @@ export default function ViewControls(props: ViewControlsProps) {
     );
   });
 
-  const wipeUpButton = !props.view ? null : <div className="wipeUpButton" onClick={showBot}><ChevronUpIcon /></div>;
+
+  // Default is a 2x2 + 1 layout meant for 4 options. Build in new possibility for 1x2 + 1 layout for 2 options
+  let optionBlockContainerStyle: React.CSSProperties = {};
+  let wipeUpButtonStyle: React.CSSProperties = {};
+  if (viewOptions.length == 2) {
+    optionBlockContainerStyle.gridTemplateColumns = "1fr";
+    wipeUpButtonStyle.gridColumn = 1;
+  }
+
+  const wipeUpButton = !props.view ? null : <div className="wipeUpButton" onClick={showBot} style={wipeUpButtonStyle}><ChevronUpIcon /></div>;
 
   return (
     <div ref={ref} className="wipeContainer">
-      <div className={`wipe ${props.view ? "hidden" : "uncollapsible"} optionBlockContainer`}>
+      <div className={`wipe ${props.view ? "hidden" : "uncollapsible"} optionBlockContainer`} style={optionBlockContainerStyle}>
         {optionBlocks}
         {wipeUpButton}
       </div>
@@ -67,26 +87,31 @@ export default function ViewControls(props: ViewControlsProps) {
 
 
 
-// NOTE: This isn't too extensible! The layout above and in the CSS is built for 4 options total
+// NOTE: This isn't too extensible! The layout above and in the CSS is built for either 2 or 4 options total
 const viewOptions = [
   {
     name: "Orbits",
-    description: "In this mode, you will be able to see an overview of the different types of orbits and their satellites.",
+    description: "See what different orbit types look like as they rotate around the Earth, as well as the ground tracks they make on the map.",
     href: "/orbits",
   },
+  // {
+  //   name: "Orbits2d",
+  //   description: "Temporary link until we implement switching within the view :)",
+  //   href: "/orbits/map",
+  // },
   {
     name: "Origin",
-    description: "In this mode, you will be able to see the most active countries in launching or active satellites throughout history.",
+    description: "Learn about which countries are the most active in the satellite space. Filter over time to see how global interest in space has evolved.",
     href: "/origin",
   },
-  {
-    name: "Launch",
-    description: "In this mode, you will see all the launch sites used depending on their interests.",
-    href: "/launch",
-  },
-  {
-    name: "Decay",
-    description: "In this mode, you will be able to point out the increase of debris number over the years.",
-    href: "/decay",
-  },
+  // {
+  //   name: "Launch",
+  //   description: "Examine the recent trend towards smaller research satellites in cheap orbits.",
+  //   href: "/launch",
+  // },
+  // {
+  //   name: "Decay",
+  //   description: "Understand the growing problem of space debris often caused by decayed satellites.",
+  //   href: "/decay",
+  // },
 ];
